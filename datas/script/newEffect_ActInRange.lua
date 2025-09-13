13b4ddf4eef98654373ff9e959d8065c83c45ecf
Crowel <faithtaeho@naver.com>
2025-09-13 16:00:00 +0900

    "XX에서 발동" 라이브러리 수정

--- datas/script/newEffect_ActInRange.lua
+++ datas/script/newEffect_ActInRange.lua
@@ -7,18 +7,50 @@ newEffect.ActInRange = {}
 EFFECT_ACT_IN_RANGE  = newEffect.newCode()
 local CheckEnabled   = false
 
+--Global Effects
+local ge1=nil
+local ge2=nil
+local ge3=nil
+local ge4=nil
+
 --local function(s)
+--ge4 : Local cost
+local actg=function(e,te,tp)
+	return te==e:GetLabelObject()
+end
+local acop_create=function(ge,te,tep,...)
+	local eff_t={...}
+	return function(e,tp,eg,ep,ev,re,r,rp)
+		--Check local cost
+		if not te then return end
+		local sel_t,res_t={},{}
+		for key,eff in ipairs(eff_t) do
+			local con=eff:GetCondition() or aux.TRUE
+			local desc=eff:GetDescription()
+			if desc==0 then desc=99 end --"Activate using a generic effect"
+			table.insert(sel_t,{con,desc})
+			table.insert(res_t,eff)
+		end
+		local sel=Duel.SelectEffect(tp,table.unpack(sel_t))
+		local re=res_t[sel]
+		local op=re and re:GetValue()
+		if op and type(op)=="function" then op(re,te,tep) end
+	end
+end
 --ge1 : Cannot cost itself
-local vctg=function(e,c)
-	local te=e:GetLabelObject()
-	return te and te:GetHandler()==c
+local vctg=function(e,tc)
+	return e:GetLabelObject()==tc
 end
 --ge2 : Activate from LOCATION_ALL
 local cpcon=function(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local te=c:GetActivateEffect()
 	if not te then return false end
-	local result=true
+	local eff_t={te:GetHandler():IsHasEffect(EFFECT_ACT_IN_RANGE)}
+	local result=(#eff_t>0)
+	if not result then return false end
+
+	--Copy effect parameters
 	e:SetDescription(te:GetDescription())
 	e:SetCategory(te:GetCategory())
 	local prop1,prop2=te:GetProperty()
@@ -37,69 +69,75 @@ local cpcon=function(e,tp,eg,ep,ev,re,r,rp)
 		e:SetCountLimit(count,{code,hopt_index},flag)
 		result=result and (te:CheckCountLimit(tp))
 	end
+	--Copy condition function
 	local con=te:GetCondition()
-	if con then result=result and con(e,tp,eg,ep,ev,re,r,rp) end
+	if con then
+		local lo=e:GetLabelObject()
+		e:SetLabelObject(te:GetLabelObject())
+		result=result and con(e,tp,eg,ep,ev,re,r,rp)
+		te:SetLabelObject(e:GetLabelObject())
+		e:SetLabelObject(lo)
+	end
+	--Create local cost operation
+	if result then
+		ge4:SetLabelObject(e)
+		ge4:SetOperation(acop_create(e,te,tp,table.unpack(eff_t)))
+	else
+		ge4:SetLabelObject(nil)
+		ge4:SetOperation(nil)
+	end
 	return result
 end
 local cpcost=function(e,tp,eg,ep,ev,re,r,rp,chk)
 	local te=e:GetHandler():GetActivateEffect()
 	local cost=te:GetCost()
 	local result=true
-	if chk==0 then
-		if cost then result=cost(e,tp,eg,ep,ev,re,r,rp,0) end
-		return result
+	--Copy cost function
+	if cost then
+		local lo=e:GetLabelObject()
+		e:SetLabelObject(te:GetLabelObject())
+		ge1:SetLabelObject(e:GetHandler())
+		result=cost(e,tp,eg,ep,ev,re,r,rp,chk)
+		ge1:SetLabelObject(nil)
+		te:SetLabelObject(e:GetLabelObject())
+		e:SetLabelObject(lo)
 	end
-	if cost then cost(e,tp,eg,ep,ev,re,r,rp,1) end
+	return result
 end
 local cptg=function(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local te=e:GetHandler():GetActivateEffect()
 	local tg=te:GetTarget()
 	local result=true
-	if chkc then
-		if tg then result=tg(e,tp,eg,ep,ev,re,r,rp,0,chkc) end
-		e:SetLabelObject(ge)
-		return result
+	--Copy target
+	if tg then
+		local lo=e:GetLabelObject()
+		e:SetLabelObject(te:GetLabelObject())
+		ge1:SetLabelObject(e:GetHandler())
+		result=tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+		ge1:SetLabelObject(nil)
+		te:SetLabelObject(e:GetLabelObject())
+		e:SetLabelObject(lo)
 	end
-	if chk==0 then
-		if tg then result=tg(e,tp,eg,ep,ev,re,r,rp,0) end
-		e:SetLabelObject(ge)
-		return result
-	end
-	if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
+	return result
 end
 local cpop=function(e,tp,eg,ep,ev,re,r,rp)
 	local te=e:GetHandler():GetActivateEffect()
 	local op=te:GetOperation()
-	if op then op(e,tp,eg,ep,ev,re,r,rp) end
+	local result=nil
+	--Copy operation
+	if op then
+		local lo=e:GetLabelObject()
+		e:SetLabelObject(te:GetLabelObject())
+		result=op(e,tp,eg,ep,ev,re,r,rp)
+		te:SetLabelObject(e:GetLabelObject())
+		e:SetLabelObject(lo)
+	end
+	return result
 end
 --ge3 : Grant effect
 local grtg=function(e,tc)
 	return tc:GetEffectCount(EFFECT_ACT_IN_RANGE)>0
 		and not (tc:IsLocation(LOCATION_HAND) or (tc:IsLocation(LOCATION_SZONE) and tc:IsFaceup()))
-end
---ge4 : Local cost
-local acop_create=function(ge,te,tep)
-	return function(e,tp,eg,ep,ev,re,r,rp)
-		if not te then return end
-		local eff_t={te:GetHandler():IsHasEffect(EFFECT_ACT_IN_RANGE)}
-		local sel_t,res_t={},{}
-		for key,eff in ipairs(eff_t) do
-			local con=eff:GetCondition() or aux.TRUE
-			local desc=eff:GetDescription()
-			if desc==0 then desc=99 end --"Activate using a generic effect"
-			table.insert(sel_t,{con,desc})
-			table.insert(res_t,eff)
-		end
-		local sel=Duel.SelectEffect(tp,table.unpack(sel_t))
-		local re=res_t[sel]
-		local op=re and re:GetValue()
-		if op and type(op)=="function" then op(re,te,tep) end
-	end
-end
-local actg=function(e,te,tp)
-	local result=te:IsHasType(EFFECT_TYPE_ACTIVATE) and te:GetHandler():GetEffectCount(EFFECT_ACT_IN_RANGE)>0
-	e:SetOperation(acop_create(e,te,tp))
-	return result
 end
 
 --public functions
@@ -107,10 +145,10 @@ function newEffect.ActInRange.EnableCheck()
 	if CheckEnabled then return end
 	CheckEnabled=true
 	--Declarations
-	local ge1=Effect.GlobalEffect()
-	local ge2=Effect.GlobalEffect()
-	local ge3=Effect.GlobalEffect()
-	local ge4=Effect.GlobalEffect()
+	ge1=Effect.GlobalEffect()
+	ge2=Effect.GlobalEffect()
+	ge3=Effect.GlobalEffect()
+	ge4=Effect.GlobalEffect()
 	--Cannot cost itself
 	ge1:SetType(EFFECT_TYPE_FIELD)
 	ge1:SetCode(EFFECT_CANNOT_USE_AS_COST)
@@ -126,7 +164,6 @@ function newEffect.ActInRange.EnableCheck()
 	ge2:SetCost(cpcost)
 	ge2:SetTarget(cptg)
 	ge2:SetOperation(cpop)
-	ge2:SetLabelObject(ge1)
 	--Grant effect
 	ge3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
 	ge3:SetTargetRange(LOCATION_ALL,LOCATION_ALL)
@@ -139,6 +176,7 @@ function newEffect.ActInRange.EnableCheck()
 	ge4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	ge4:SetTargetRange(1,1)
 	ge4:SetTarget(actg)
+	ge4:SetLabelObject(nil)
 	Duel.RegisterEffect(ge4,0)
 end
 function newEffect.ActInRange.LimitCon()
@@ -149,6 +187,8 @@ end
 function newEffect.ActInRange.LimitOp()
 	return function(e,te,tp)
 		Duel.Hint(HINT_CARD,0,e:GetOwner():GetOriginalCode())
+		--Duel.HintSelection(e:GetHandler(),true)
+
 		e:UseCountLimit(e:GetHandlerPlayer(),1)
 	end
 end
