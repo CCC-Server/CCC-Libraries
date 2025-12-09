e01b362079415249118225ba4e0b9e1cc2c09afa
Crowel <faithtaeho@naver.com>
2025-12-09 19:00:00 +0900

    최초 패 관련 라이브러리 수정 중 (중간저장)

--- datas/script/proc_skill_links.lua
+++ datas/script/proc_skill_links.lua
@@ -23,27 +23,11 @@ Auxiliary.SetSkillOp = function(coverNum,skillcon,skillop,countlimit,efftype)
 	end
 end
 
--- Proc for skills that determines starting hand
--- c: the card (card)
--- coverId: the Id of the cover (int)
--- include_filter: card filter that returns true if a card must be included to starting hand (function or nil)
--- exclude_filter: card filter that returns true if a card must be excluded to starting hand (function or nil)
--- skillcon: condition to activate the skill (function or nil)
--- skillop: operation related to the skill activation, mostly oath (function or nil)
-Auxiliary.AddStartingHandsSkillProcedure = aux.FunctionWithNamedArgs(
-function(c,coverNum,include_filter,exclude_filter,skillcon,skillop)
-	--To do...
-
-end,"handler","coverNum","include_filter","exclude_filter","skillcon","skillop")
-
--- Proc for Deck Recipe condition
--- min1: can be omitted, 1 as default (int)
--- min2: can be omitted, nil as default (int)
--- max1: can be omitted, nil as default (int)
--- max2: can be omitted, 0 as default (int)
--- if min1 is another filter (function), this condition is regarded as function(con,functions...,mins...,maxs...) form, with same amounts of arguments each
-Auxiliary.CreateDeckRecipeCondition = aux.FunctionWithNamedArgs(
-function(con,fil1,fil2,min1,min2,max1,max2,...)
-	--To do...
-	
-end,"skillcon","include_filter","exclude_filter","include_min","exclude_min","include_max","exclude_max")
+-- replace starting hand
+local StartingHandCondition={
+	[0]=aux.FALSE,
+	[1]=aux.FALSE
+}
+function Auxiliary.AddStartingHandCondition(tp,f)
+end
+-- To do...
